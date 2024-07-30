f6edc3f088419e9b30c2913d8e2b814bc8a63af2
nzt-wrk <nzt.rus@gmail.com>
2024-07-31 00:55:30 +0300

    Add files via upload

---
+++ module_1_1.py
@@ -0,0 +1,14 @@
+example='Стюардесса'
+print(example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[1::2])
+print()
+
+example='Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[1:-1:2])
