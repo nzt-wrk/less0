557b2fe0553980794a0549e68fda6a2e5cd6877c
nzt-wrk <nzt.rus@gmail.com>
2024-07-30 21:31:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# задача 1
+print('1st program')
+print(9**0.5*5)
+print()
+
+# задача 2
+print('2st program')
+print(9.99 > 9.98 and 1000 != 1000.1)
+print()
+
+# задача 3
+print('3rd program')
+print(1234//10%100+5678//10%100)
+print()
+
+# задача 4
+print('4rd program')
+print(int(13.42) == 13 and int(42.13*100%100) == 13)
+print()
+
+# test
+print(float(4213//100), 4213//100, int(4213//100), 4213/100)
+print(float(4213%100), 4213%100, float(42.13%100))
