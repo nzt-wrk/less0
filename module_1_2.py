ae6f281877ef7c8294bbbeadea353ae2e51beb21
nzt-wrk <nzt.rus@gmail.com>
2024-07-31 15:10:48 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,11 @@
+# Переменные
+
+DZ_count = 12
+Time = 1.5
+Kurs_name = 'Python'
+Average_time = Time/DZ_count
+
+print('\nКурс: '+Kurs_name,
+      '\nВсего задач:', DZ_count,
+      '\nЗатрачено часов:', Time,
+      '\nCреднее время выполнения:',Average_time)
