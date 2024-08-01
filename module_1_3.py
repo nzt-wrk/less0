663f35657594a4a40672f33b8d12c6b0e2270ac0
nzt-wrk <nzt.rus@gmail.com>
2024-08-01 14:59:08 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,9 +1,11 @@
 name = 'Stepan'
-age = 37;
-age = age+1;
-is_student = True
-
 print('Name: '+name)
+age = 37;
 print('Age:',age)
+age = age+1;
 print('New age:',age)
+is_student = True
 print('Is student:',is_student)
+
+
+
